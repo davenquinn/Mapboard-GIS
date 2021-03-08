faf0a5207c8d9d07afdc4e8563e48b03caf9f3b8
Daven Quinn <dev@davenquinn.com>
2021-03-07 22:38:26 -0600

    Updated revision info plugin

--- next.config.js
+++ next.config.js
@@ -1,19 +1,26 @@
-const withStylus = require('@zeit/next-stylus');
-const admonitions = require("remark-admonitions")
-const withMDX = require('@next/mdx')({
+const withStylus = require("@zeit/next-stylus");
+const admonitions = require("remark-admonitions");
+const withMDX = require("@next/mdx")({
   options: {
     remarkPlugins: [admonitions],
     rehypePlugins: [],
   },
 });
-const withCoffeescript = require('next-coffeescript');
+const withCoffeescript = require("next-coffeescript");
+const RevisionInfoWebpack = require("@macrostrat/revision-info-webpack");
+const pkg = require("./package.json");
 
-const isProd = process.env.NODE_ENV === 'production'
+const GITHUB_LINK = "https://github.com/davenquinn/Mapboard-GIS";
+
+const isProd = process.env.NODE_ENV === "production";
 
 let baseCfg = {
-  pageExtensions: ['js', 'jsx', 'mdx'],
+  pageExtensions: ["js", "jsx", "mdx"],
   env: {
-    MEDIA_PATH: isProd ? "//sfo2.digitaloceanspaces.com/mapboard-gis-assets" : "/media",
+    MEDIA_PATH: isProd
+      ? "//sfo2.digitaloceanspaces.com/mapboard-gis-assets"
+      : "/media",
+    ...RevisionInfoWebpack(pkg, GITHUB_LINK),
   },
 };
 
